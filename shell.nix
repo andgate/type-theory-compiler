0b70aeaf415da31e8a1100eafc0bd543d69c4bcf
Gabe Anderson <gabeanderson@MACBOOKPRO-B1FF.home>
2019-07-14 19:50:18 -0700

    shell.nix is updated for macos, but still broken for some reason...

--- shell.nix
+++ shell.nix
@@ -14,20 +14,45 @@ let
     sha256 = "1lvz85yc7s0yzix3hbcl9m8ahps1jdr9v2sbyq0phfawwjbgswyd";
   };
 
+  darwin_pkgs18 = hostpkgs.fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs-channels";
+    rev = "nixpkgs-18.03-darwin";
+    sha256 = "12f7aiwajvxcpi351yh6yg9l30xivn44k6rj5lk558q0gmc5j0p1";
+  };
+
+  darwin_pkgs = hostpkgs.fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs-channels";
+    rev = "nixpkgs-19.03-darwin";
+    sha256 = "01va89pqny7jh9iic3f6dmywpc5m4gf6v106fsgysg50ghkg1gbb";
+  };
+
+  darwin_unstable_pkgs = hostpkgs.fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs-channels";
+    rev = "nixpkgs-unstable";
+    sha256 = "01pfzhghkpsmbr0v1754gjzdlk5fgs6v1vwmplnh87vjjm6xka1n";
+  };
+
+  pkgs18 = import darwin_pkgs18 {};
+
   pkgs = if hostpkgs.stdenv.isDarwin
-          then import nixos_pkgs {}
+          then import darwin_pkgs {}
           else import nixos_pkgs {};
+
   upkgs = if hostpkgs.stdenv.isDarwin
-          then import nixos_unstable_pkgs {}
+          then import darwin_unstable_pkgs {}
           else import nixos_unstable_pkgs {};
 
   nixPackages = [
-    pkgs.ghc
-    pkgs.cabal-install
+    pkgs.haskellPackages.ghc_8_6_1
+    pkgs.haskellPackages.cabal-install
     pkgs.less
     pkgs.git
+    pkgs.gcc
     upkgs.llvm_8
-    pkgs.clang_7
+    upkgs.clang_7
     pkgs.gdb
   ];
 in
